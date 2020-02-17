fbacd9fc8ddda17dc44a5e3328856470fe4ce945
Vasyl Matsevko <matsevko.vasil@gmail.com>
2020-02-17 11:06:20 +0200

    second comitte

---
+++ build/precache-manifest.0a3d54c4d8062468df67fc4b97ce45be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c985790dadce261674c066589baae45",
+    "url": "/react-form/build/index.html"
+  },
+  {
+    "revision": "7cc5adbc9674dc4936f5",
+    "url": "/react-form/build/static/css/main.b9231a23.chunk.css"
+  },
+  {
+    "revision": "965fb8f9aa0d2873dbf8",
+    "url": "/react-form/build/static/js/2.e5bed5d5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-form/build/static/js/2.e5bed5d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cc5adbc9674dc4936f5",
+    "url": "/react-form/build/static/js/main.de9af2f2.chunk.js"
+  },
+  {
+    "revision": "ea3574ceeb464bbf0577",
+    "url": "/react-form/build/static/js/runtime-main.f4958f10.js"
+  }
+]);
